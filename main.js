165fcb32260f4c4ca7588d4901af087e00f62656
Muhammad <miranshoh108@gamil.com>
2024-12-04 01:06:03 +0500

    done

---
+++ main.js
@@ -0,0 +1,20 @@
+//1-masala
+// let son = "salom";
+// let re = "";
+// for (let i = 0; i <= son.length - 1; i++) {
+//   re += son[i] + "";
+// }
+// console.log(re);
+//2-masala
+// let soz1 = "abc";
+// let soz2 = "cba";
+// let soz3 = "";
+// for (let i = soz2.length - 1; i >= 0; i--) {
+//   soz3 += soz2[i];
+// }
+// if (soz3 == soz1) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
